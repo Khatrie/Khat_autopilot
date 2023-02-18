1c1b200ef471efcbb79bef67244ba639e970fcda
Khatrie <Fire666dev@gmail.com>
2023-02-18 03:47:14 -0500

    Revamp
    Disables AP when applying Brakes,
    Slow Car when getting close to destination
    auto apply brakes based on driving mode
    Disables AP on Collision, start car alarm upon collision

--- client/client.lua
+++ client/client.lua
@@ -1,125 +1,149 @@
------------------For support, scripts, and more----------------
---------------- https://discord.gg/wasabiscripts  -------------
----------------------------------------------------------------
-
-local Speed = Config.NormalSpeed
-local Drive = Config.NormalDrive
-local mode = "normal"
-
-if Config.NormalKeybind then
-    RegisterCommand('+normalpilot', function()
-        local player = PlayerPedId()
-        if IsPedInAnyVehicle(player) then
-            mode = "normal"
-            Drive = Config.NormalDrive
-        end
-        if DoesBlipExist(GetFirstBlipInfoId(8)) and IsPedInAnyVehicle(player) then
-            local blip = GetFirstBlipInfoId(8)
-            local bCoords = GetBlipCoords(blip)
-            ClearPedTasks(player)
-            local veh = GetVehiclePedIsIn(player, false)
-            TaskVehicleDriveToCoord(player, veh, bCoords, tonumber(Speed), 0, veh, Drive, 0, true)
-            SetDriveTaskDrivingStyle(player, Drive)
-            if Config.MythicNotify then
-                exports['mythic_notify']:SendAlert('success', Config.Translate[5], 2500)
-            elseif Config.pNotify then
-                exports['pNotify']:SendNotification({text = Config.Translate[5], type = "success", 2500})
-            end
-        end
-    end, false)
-
-    RegisterCommand('-normalpilot', function()
-        if IsPedInAnyVehicle(PlayerPedId()) then
-            ClearPedTasks(PlayerPedId())
-        end
-    end, false)
-    RegisterKeyMapping('+normalpilot', 'Autopilot(Normal Driving)', 'keyboard', Config.DefaultKey)
-end
-
-if Config.CrazyKeybind then
-    RegisterCommand('+crazypilot', function()
-        local player = PlayerPedId()
-        if IsPedInAnyVehicle(player) then
-            mode = "crazy"
-            Drive = Config.CrazyDrive
-        end
-        if DoesBlipExist(GetFirstBlipInfoId(8)) and IsPedInAnyVehicle(player) then
-            local blip = GetFirstBlipInfoId(8)
-            local bCoords = GetBlipCoords(blip)
-            ClearPedTasks(player)
-            local veh = GetVehiclePedIsIn(player, false)
-            TaskVehicleDriveToCoord(player, veh, bCoords, tonumber(Speed), 0, veh, Drive, 0, true)
-            SetDriveTaskDrivingStyle(player, Drive)
-            if Config.MythicNotify then
-                exports['mythic_notify']:SendAlert('success', Config.Translate[6], 2500)
-            elseif Config.pNotify then
-                exports['pNotify']:SendNotification({text = Config.Translate[6], type = "success", 2500})
-            end
-        end
-    end, false)
-
-    RegisterCommand('-crazypilot', function()
-        if IsPedInAnyVehicle(PlayerPedId()) then
-            ClearPedTasks(PlayerPedId())
-        end
-    end, false)
-    RegisterKeyMapping('+crazypilot', 'Autopilot(Agressive Driving)', 'keyboard', Config.CrazyKey)
-end
+local Speed = 35.0
+local Drive = 786603
+local useDebug = false
+--DO NOT TOUCH
+local autopilot = false
+local bCoords = nil
+local LastVehicleHealth = nil
+--
 
 RegisterCommand(Config.StartCommand, function(source, args)
+    local ev = 0
     local player = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn(player, false)
+    print(QBCore.Debug(args))
+    for i in pairs(Config.APVehicles) do
+        local vehmodel = IsVehicleModel(vehicle, Config.APVehicles[i])
+        if vehmodel then
+            if IsPedInAnyVehicle(player) then
+                if table.concat(args," ") == 'normal' then
+                    Drive = Config.NormalDrive
+                    Speed = Config.NormalSpeed
+                elseif table.concat(args," ") == 'crazy'then
+                    Drive = Config.CrazyDrive
+                    Speed = Config.CrazySpeed
+                end
+                if DoesBlipExist(GetFirstBlipInfoId(8)) then
+                    local blip = GetFirstBlipInfoId(8)
+                    bCoords = GetBlipCoords(blip)
+                    ClearPedTasks(player)
+                    local veh = GetVehiclePedIsIn(player, false)
+                    TaskVehicleDriveToCoord(player, veh, bCoords.x, bCoords.y, bCoords.z, tonumber(Speed), 0, veh, Drive, 25.0, true)
+                    SetDriveTaskDrivingStyle(player, Drive)
+                    LastVehicleHealth = GetVehicleBodyHealth(vehicle)
 
-    if IsPedInAnyVehicle(player) then
-        if table.concat(args," ") == Config.NormalCMD then
-            mode = "normal"
-        end
-        if table.concat(args," ") == Config.CrazyCMD then
-            mode = "crazy"
-        end
-        if mode == 'crazy' then
-            Drive = Config.CrazyDrive
-            Speed = Config.CrazySpeed
-        elseif mode == 'normal' then
-            Drive = Config.NormalDrive
-        end
-        if DoesBlipExist(GetFirstBlipInfoId(8)) then
-            local blip = GetFirstBlipInfoId(8)
-            local bCoords = GetBlipCoords(blip)
-            ClearPedTasks(player)
-            local veh = GetVehiclePedIsIn(player, false)
-            TaskVehicleDriveToCoord(player, veh, bCoords, tonumber(Speed), 0, veh, Drive, 0, true)
-            SetDriveTaskDrivingStyle(player, Drive)
+                    autopilot = true
 
-            if Config.MythicNotify then
-                exports['mythic_notify']:SendAlert('success', Config.Translate[1], 2500)
-            elseif Config.pNotify then
-                exports['pNotify']:SendNotification({text = Config.Translate[1], type = "success", 2500})
+                    QBCore.Functions.Notify(Config.Translate[1], 'success')
+                else
+                    QBCore.Functions.Notify(Config.Translate[2], 'error')
+                end
+            else
+                QBCore.Functions.Notify(Config.Translate[3], 'error')
             end
-
-        elseif Config.pNotify then
-            exports['pNotify']:SendNotification({text = Config.Translate[2], type = "error", timeout = 2500})
-        elseif Config.MythicNotify then
-            exports['mythic_notify']:SendAlert('error', Config.Translate[2], 2500)
+        else
+            ev = ev + 1
         end
-
-    elseif Config.pNotify then
-        exports['pNotify']:SendNotification({text = Config.Translate[3], type = "error", timeout = 2500})
-    elseif Config.MythicNotify then
-        exports['mythic_notify']:SendAlert('error', Config.Translate[3], 2500)
+    end
+    if ev == 9 then
+        QBCore.Functions.Notify(Config.Translate[8], 'error')
     end
 end, false)
 
-RegisterCommand(Config.StopCommand, function(source, args)
+RegisterCommand(Config.StopCommand, function()
     if IsPedInAnyVehicle(PlayerPedId()) then
         ClearPedTasks(PlayerPedId())
-        if Config.MythicNotify then
-            exports['mythic_notify']:SendAlert('error', Config.Translate[4], 2500)
-        elseif Config.pNotify then
-            exports['pNotify']:SendNotification({text = Config.Translate[4], type = "error", timeout = 2500})
-        end
-    elseif Config.MythicNotify then
-        exports['mythic_notify']:SendAlert('error', Config.Translate[3], 2500)
-    elseif Config.pNotify then
-        exports['pNotify']:SendNotification({text = Config.Translate[3], type = "error", timeout = 2500})
+        autopilot = false
+        bCoords = nil
+        called = false
+        QBCore.Functions.Notify(Config.Translate[4], 'success')
+    else
+        QBCore.Functions.Notify(Config.Translate[3], 'error')
     end
 end, false)
+
+Citizen.CreateThread(function()
+    local ped = PlayerPedId()
+
+    local sleep = 1000
+    local vehicle = GetVehiclePedIsIn(ped, false)
+    local called = false
+    while true do
+        if autopilot and bCoords ~= nil then
+            sleep = 1
+            local coords = GetEntityCoords(ped)
+            local x = coords.x - bCoords.x
+            local y = coords.y - bCoords.y
+            if x <= 0 then
+                x = math.abs(x)
+            end
+            if y <= 0 then
+                y = math.abs(y)
+            end
+            if useDebug then
+                print("Coords: ".. coords, "| Bcoords: ".. bCoords)
+                print("X: ".. x, "| Y: ".. y)
+            end
+            if x <= 30 and y <= 30 then
+                if IsPedInAnyVehicle(PlayerPedId()) then
+                    if not called then
+                        ClearPedTasks(ped)
+                        QBCore.Functions.Notify(Config.Translate[12], 'success')
+                        TaskVehicleDriveToCoord(ped, vehicle, bCoords.x, bCoords.y, bCoords.z, 15.0, 0, vehicle, 8388614, 0.0, true)
+                        called = true
+                    end
+                    if x < 10 and y < 10 then
+                        QBCore.Functions.Notify(Config.Translate[7], 'success')
+                        ClearPedTasks(PlayerPedId())
+                        SetVehicleBrake(vehicle, true)
+                        if Drive == '786603' then
+                            Wait(1500)
+                        else
+                            Wait(3000)
+                        end
+                        SetVehicleBrake(vehicle, false)
+                        called = false
+                        autopilot = false
+                        bCoords = nil
+                        sleep = 1000
+                    end
+                end
+            end
+
+            if IsControlJustPressed(1, 72) then
+                if IsPedInAnyVehicle(PlayerPedId()) then
+                    ClearPedTasks(PlayerPedId())
+                    QBCore.Functions.Notify(Config.Translate[9], 'success')
+                    autopilot = false
+                    bCoords = nil
+                    sleep = 1000
+                end
+            end
+
+            if not IsPedInAnyVehicle(PlayerPedId()) then
+                ClearPedTasks(PlayerPedId())
+                    autopilot = false
+                    bCoords = nil
+                    sleep = 1000
+            end
+
+            local health = GetVehicleBodyHealth(vehicle)
+            if health < LastVehicleHealth then
+                ClearPedTasks(PlayerPedId())
+                SetVehicleAlarm(vehicle, true)
+                StartVehicleAlarm(vehicle)
+                SetVehicleAlarmTimeLeft(vehicle, math.random(3000,8000))
+                autopilot = false
+                bCoords = nil
+                sleep = 1000
+                QBCore.Functions.Notify(Config.Translate[10], 'success')
+                Wait(3000)
+                QBCore.Functions.Notify(Config.Translate[11], 'error')
+
+
+            end
+
+        end
+        Wait(sleep)
+    end
+end)
