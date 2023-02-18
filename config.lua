7869d3b3a164ca0632cf5c05453045f4daf0b106
Khatrie <Fire666dev@gmail.com>
2023-02-18 03:47:52 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,30 +1,12 @@
------------------For support, scripts, and more----------------
---------------- https://discord.gg/wasabiscripts  -------------
----------------------------------------------------------------
-
 Config = {}
-
-Config.checkForUpdates = true -- Check for Updates?
-
-Config.NormalSpeed = 25.0 -- Normal Speed
+Config.NormalSpeed = 35.0 / 1.6 -- Normal Speed
 Config.NormalDrive = 786603 -- Normal Drive Style
-Config.CrazySpeed = 100.0 -- Crazy Speed
+Config.CrazySpeed = 150.0 / 1.6 -- Crazy Speed
 Config.CrazyDrive = 1074528293 -- Crazy Drive Style
-
-Config.NormalKeybind = true -- Enable Keymapping for toggling normal driving autopilot
-Config.DefaultKey = "i" -- Default keybind to toggle normal driving autopilot
-Config.CrazyKeybind = true -- Enable Keymapping for toggling crazy driving autopilot
-Config.CrazyKey = "o" -- Default keybind to toggle crazy driving autopilot
-
-Config.StartCommand = "autopilot" -- Command to activate Auto-Pilot
-Config.NormalCMD = "normal" -- Normal driving command argument
-Config.CrazyCMD = "crazy" -- Crazy driving command argument
+Config.StartCommand = "ap" -- Command to activate Auto-Pilot
 --Default normal autopilot: /autopilot normal
 
 Config.StopCommand = "apstop" -- Command to disable Auto-Pilot
-
-Config.MythicNotify = true --If you use Mythic Notify
-Config.pNotify = false --If you use pNotify
 
 Config.Translate = {
     [1] = "Autopilot - Activated",
@@ -32,5 +14,23 @@ Config.Translate = {
     [3] = "You are not in a vehicle!",
     [4] = "Autopilot - Disabled",
     [5] = "Autopilot Normal - Button Pressed",
-    [6] = "Autopilot Aggressive - Button Pressed"
+    [6] = "Autopilot Aggressive - Button Pressed",
+    [7] = "Destination Reached, Autopilot Disabled.",
+    [8] = "Not An Electric Vehicle",
+    [9] = "Manual Override Detected.",
+    [10] = "Accident Detected, Autopilot Disabled!",
+    [11] = "Accident Reported!",
+    [12] = "Approaching Destination, Brakes will Automatically Apply.",
+}
+
+Config.APVehicles = {
+    'raiden',
+    'voltic',
+    'neon',
+    'cyclone',
+    'virtue',
+    'omnisegt',
+    'cyclone2',
+    'dilettante',
+    'dilettante2'
 }
